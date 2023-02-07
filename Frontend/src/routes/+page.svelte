6c71fa679a0e4363a1ec0ae2c3fcb916ab3257a8
Sam Perry <samperry.dev@gmail.com>
2023-02-07 23:03:03 +0000

    wip styling

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col h-screen ">
+    <section class="justify-center mx-auto">
+        <div class="text-4xl font-extrabold ...">
+            <span class="bg-clip-text text-transparent  underline decoration-sky-500" style="text-shadow:-1px -1px 0 #faa,  
+                1px -1px 0 #fff,
+                -1px 1px 0 #000,
+                1px 1px 0 #000;">
+            Hi I'm Sam the SoftwareStrongman.
+            </span>
+            <div class="text-2xl text-transparent"  style="text-shadow:-1px -1px 0 #fff,  
+                1px -1px 0 #b0978b,
+                -1px 1px 0 #b0978b,
+                1px 1px 0 #b0978b;">
+                This site is my personal tech blog and workout planner free for public use.
+            </div>
+        </div>
+        <small class="text-gray-400  text-xs">Hopefully there isn't but there may be a few bugs, if you stumble upon one please report them using the <a href="/contactMe" class="text-blue-600 hover:text-blue-300 transition">contact me</a> page and submitting a bug report.</small>
+    </section>
+    <section class="my-12">
+        <h1>About me</h1>
+    </section>
+
+    <section class="my-12 flex flex-row-reverse">
+        <h1>My Projects</h1>
+    </section>
+</div>
--- Frontend/src/routes/Register/+page.svelte
+++ Frontend/src/routes/Register/+page.svelte
@@ -4,31 +4,34 @@
 
 
 </script>
-<div class="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8  rounded-lg min-h-full">
+<div class="flex flex-col items-center lg:justify-center px-4 sm:px-6 lg:px-8  rounded-lg h-screen ">
+  <div>
+
+
   <h1 class="font-bold font-mono mt-2">Register</h1>
   <small>Sign Up to access the free workout planning app.</small>
-    <form class="mb-8 space-y-6 w-full lg:w-6/12" action="#" method="POST" use:enhance={() => {
-      return async ({ result }) => {
-        pb.authStore.loadFromCookie(document.cookie);
-        await applyAction(result);
-      }
-    }}>
+  <form class="mb-8 space-y-6 w-full " action="#" method="POST" use:enhance={() => {
+    return async ({ result }) => {
+      pb.authStore.loadFromCookie(document.cookie);
+      await applyAction(result);
+    }
+  }}>
       <input type="hidden" name="remember" value="true">
       <div class="space-y-2 rounded-md shadow-sm">
         
-          <input id="firstName" name="firstName" type="text" autocomplete="first name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="First Name">
-
-          <input id="lastName" name="lastName" type="text" autocomplete="last name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Last Name">
-
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address">
+        <input id="firstName" name="firstName" type="text" autocomplete="first name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="First Name">
+        
+        <input id="lastName" name="lastName" type="text" autocomplete="last name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Last Name">
+        
+        <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address">
      
-          <input id="password" name="password" type="password"  required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password">
-
-          <input id="passwordConf" name="passwordConfirm" type="password"  required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Re-enter Password">
+        <input id="password" name="password" type="password"  required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password">
+        
+        <input id="passwordConf" name="passwordConfirm" type="password"  required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Re-enter Password">
        
       </div>
-
-       <small>By Signing up you agree to our data terms and conditions which can be found here <a href="/Privacy" class="text-blue-500 hover:text-blue-600">T&C's</a></small>
+      
+      <small>By Signing up you agree to our data terms and conditions which can be found here <a href="/Privacy" class="text-blue-500 hover:text-blue-600">T&C's</a></small>
       <button type="submit" class="mb-5 group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">        
           <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -37,6 +40,9 @@
         </span>
         Sign Up
       </button>
-
+      
     </form>
-</div>
+    </div>
+    
+  </div>
+  
